3286fedac284afa62d93a0697a1d6ec821a08c45
ilham <ilhamsyihabudin791@gmail.com>
2023-10-14 21:09:59 +0700

    ilham

---
+++ pertemuan 1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+
+    cout<<"Hello, World!\n\n\n"<<endl;
+    cout<<"--------------------------------------------\n"<<endl;
+    cout<<"Nama :Ilham Syihabudin\n"<<endl;
+    cout<<"NPM : 2310631170158\n"<<endl;
+    cout<<"Kelas : B - Informatika\n"<<endl;
+    cout<<"Cita - cita : Data Analyst\n"<<endl;
+    cout<<"Motivasi masuk Informatika : Keren\n"<<endl;
+    cout<<"--------------------------------------------\n"<<endl;
+    return 0;
+}
